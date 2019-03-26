f346efff81e3d7d9595e4e12c06dc682f60ed00e
orhgool <orhgool@gmail.com>
2019-03-26 11:57:34 -0600

    postgres

--- itoolboxpro/settings.py
+++ itoolboxpro/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'itoolboxpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zorro',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
